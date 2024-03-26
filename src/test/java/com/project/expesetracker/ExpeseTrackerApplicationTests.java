df39d531ddf68bf841950330f76215e89f59c93d
Vjchavan <vijaychavan582000@gmail.com>
2024-03-26 21:15:20 +0530

    Upload project structure

---
+++ src/test/java/com/project/expesetracker/ExpeseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.expesetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpeseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
